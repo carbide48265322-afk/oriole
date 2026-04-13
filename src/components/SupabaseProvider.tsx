2cb9c41049c960cc59d997605f012e765a03e7d5
方研 <15867139271@163.com>
2026-04-13 17:33:02 +0800

    添加环境变量调试日志

--- src/components/SupabaseProvider.tsx
+++ src/components/SupabaseProvider.tsx
@@ -15,7 +15,12 @@ export function SupabaseProvider({ children }: { children: ReactNode }) {
   const [session, setSession] = useState<Session | null>(null)
 
   useEffect(() => {
+
     if (supabase) {
+      console
+        .log(
+          'URL'
+          , process.env.NEXT_PUBLIC_SUPABASE_URL)
       // 监听会话变化
       const { data: { subscription } } = supabase.auth.onAuthStateChange((_event, session) => {
         setSession(session)
